2d1fa948a6e9b1b42b20fb2fdc9eef9249fe38a8
Sys-adm1n-headHunter <greatest.sa.hh.2022@gmail.com>
2022-08-18 05:47:22 +0300

    add Task1

--- day1/Task1.java
+++ day1/Task1.java
@@ -10,7 +10,7 @@ JAVA JAVA JAVA JAVA JAVA JAVA JAVA JAVA JAVA JAVA
 public class Task1 {
     public static void main(String[] args) {
         int i = 0;
-        while (i<10){
+        while (i < 10) {
             System.out.print("JAVA ");
             i++;
         }
