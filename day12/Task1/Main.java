b83fa89f92048351452b7d59ada497806e039dd1
Sys-adm1n-headHunter <greatest.sa.hh.2022@gmail.com>
2022-11-09 22:59:42 +0300

    add day 12 Task1

---
+++ day12/Task1/Main.java
@@ -0,0 +1,27 @@
+package day12.Task1;
+/* SAI
+1. Создать список строк, добавить в него 5 марок автомобилей, вывести список в
+консоль. Добавить в середину еще 1 автомобиль, удалить самый первый автомобиль
+из списка. Распечатать список.
+ */
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList <String> cars = new ArrayList<String>();
+        cars.add("BMW");
+        cars.add("Lada");
+        cars.add("Ferrari");
+        cars.add("Bugatti");
+        cars.add("Ford");
+        System.out.println(cars);
+
+        cars.add(3,"Audi");
+        cars.remove(0);
+        System.out.println(cars);
+
+
+
+    }
+}
