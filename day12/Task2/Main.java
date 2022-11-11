f0d75112f7619a98249320e4292e9cc7efc1a190
Sys-adm1n-headHunter <greatest.sa.hh.2022@gmail.com>
2022-11-11 07:11:29 +0300

    add day 12 Task2

---
+++ day12/Task2/Main.java
@@ -0,0 +1,21 @@
+package day12.Task2;
+/* SAI
+Создать новый список, заполнить его четными числами от 0 до 30 и от 300 до 350.
+Вывести список.
+ */
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        for (int i = 0; i <= 350; i++) {
+            if(0<=i && i<=30 ){
+                arr.add(i);
+            }else if (300<=i && i<=350){
+                arr.add(i);
+            }
+        }
+        System.out.println(arr);
+    }
+}
